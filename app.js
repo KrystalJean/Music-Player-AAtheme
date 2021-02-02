a4f651ed4701bad21b3005afe73da55896bd12a4
Krystal Jean Austin <44277065+KrystalJean@users.noreply.github.com>
2021-02-02 17:51:45 -0600

    Init commit. Beginning with a previous template that I built. Will design a new skin/theme/mp3s beginning at this point.

---
+++ app.js
@@ -0,0 +1,107 @@
+const musicContainer = document.getElementById('music-container');
+
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('music-cover');
+
+// Song Titles
+const songs = ['littleidea', 'jazzyfrenchy', 'ukulele' ];
+
+//Keep track of song
+let songIndex = 2;
+
+//Load song details into Dom
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `img/${song}.jpg`;
+}
+
+//Play Song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play();
+}
+
+//Pause Song
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
+}
+
+//Previous Song
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+//Next Song
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length -1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+//Update Progress Bar
+function updateProgress(e) {
+    const {duration,currentTime} = e.srcElement;
+    const progressPercent = (currentTime/duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+//Set Progress Bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX/width) * duration;
+}
+
+//Event Listener
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+
+//Change Song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+//Time&Song update
+audio.addEventListener('timeupdate', updateProgress);
+
+//Click on Progress bar
+progressContainer.addEventListener('click', setProgress);
+
+//Song Ends
+audio.addEventListener('ended', nextSong);
